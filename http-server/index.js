e94df02085567c5878f97825d4d3fe2ce407a63e
anju1369 <103201221+anju1369@users.noreply.github.com>
2023-02-28 00:17:32 +0530

    Add files via upload

---
+++ http-server/index.js
@@ -0,0 +1,51 @@
+const http = require("http");
+const fs = require("fs");
+const args = require("minimist")(process.argv);
+let hc = "";
+let pc = "";
+let rc = "";
+
+fs.readFile("home.html", (errors, home) => {
+  if (errors) {
+    throw errors;
+  }
+  else{
+    hc = home;
+  }
+  
+});
+
+fs.readFile("project.html", (errors, project) => {
+  if (errors) {
+    throw errors;
+  }
+  pc = project;
+});
+
+fs.readFile("registration.html", (errors, registration) => {
+  if (errors) {
+    throw errors;
+  }
+  rc = registration;
+});
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(pc);
+        response.end();
+        break;
+      case "/registration.html":
+        response.write(rc);
+        response.end();
+        break;
+      default:
+        response.write(hc);
+        response.end();
+        break;
+    }
+  })
+  .listen(args.port);
